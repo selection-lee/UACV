d72083eb428c4f267d32bd5480d125c0b06ebf02
송준혁 <fprhqkrtk303@naver.com>
2024-07-28 22:38:04 +0900

    feat: MongoDB에 센서 데이터 저장 [1/2]
    TODO: Timestamp 추가
    
    Issue ID: S11P12C102-147

--- backend-test/src/main/java/account/spring/hardware/dto/SensorDataDto.java
+++ backend-test/src/main/java/account/spring/hardware/dto/SensorDataDto.java
@@ -1,19 +1,27 @@
 package account.spring.hardware.dto;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@Getter
+@Data
 @ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+// @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// @RequiredArgsConstructor
 public class SensorDataDto {
+
+    // private Long deviceId;
+
+    private LocalDateTime receivedDate;
 
     // 차체 방향
     private Float chassisDir;
@@ -22,17 +30,15 @@ public class SensorDataDto {
     // DC 모터 제어값
     private Float throttleValue;
     // 온습도
-    private Float tempeature;
+    private Float temperature;
+    // TODO
     // 라이다 / 맵핑 데이터
-    @CreationTimestamp
-    private Date receivedDate;
-    
-    @Builder
-    public SensorDataDto(Float chassisDir, Float cannonDir, Float tempeature, Float throttleValue, Date receivedDate) {
-        this.chassisDir = chassisDir;
-        this.cannonDir = cannonDir;
-        this.tempeature = tempeature;
-        this.throttleValue = throttleValue;
-        this.receivedDate = receivedDate;
-    }
+
+    // public SensorDataDto(Long deviceId, Float chassisDir, Float cannonDir, Float throttleValue, Float tempeature) {
+    //     this.deviceId = deviceId;
+    //     this.chassisDir = chassisDir;
+    //     this.cannonDir = cannonDir;
+    //     this.throttleValue = throttleValue;
+    //     this.tempeature = tempeature;
+    // }
 }
