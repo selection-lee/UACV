5141e55b7835d60e49d39473a2ab58e1c85a6beb
송준혁 <fprhqkrtk303@naver.com>
2024-08-07 21:57:17 +0900

    refactor: DeviceController로 이름 변경
    SecurityConfig에 /api 접근 허용
    
    Issue ID: S11P12C102-330

--- uacv_backend/src/main/java/uacv/backend/hardware/api/DeviceController.java
+++ uacv_backend/src/main/java/uacv/backend/hardware/api/DeviceController.java
@@ -7,22 +7,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import uacv.backend.hardware.dto.ControlDataDto;
+import uacv.backend.hardware.service.ReceiveService;
 import uacv.backend.hardware.service.SendService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
-@RequestMapping("/api/v1/mqtt")
-public class SendController {
-    
+@RequestMapping("/api/device")
+public class DeviceController {
+
+    @Autowired
+    private ReceiveService receiveService;
+
     @Autowired
     private SendService sendService;
 
-    @PostMapping("/send")
+    // 센서 정보 수신?
+    // @PostMapping("/receive")
+    // public String postMethodName(@RequestBody String entity) {
+    //     return entity;
+    // }
+
+
+    // 로그 기록 조회
+    @GetMapping("/logs")
     public ResponseEntity<?> sendMessage(@RequestBody ControlDataDto controlDataDto) {
         sendService.sendMessage(controlDataDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    // 조종 명령 송신
+
+
+    // 조종 명령 수행 완료 수신
+    
+
 }
