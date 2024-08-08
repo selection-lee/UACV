815b2214d0018f72267e9aa790d6dfd8e22de380
송준혁 <fprhqkrtk303@naver.com>
2024-08-08 11:56:38 +0900

    feat: 대시보드
    로그인 메인페이지 수정
    
    Issue ID: S11P12C102-408

--- uacv_frontend/src/pages/login.vue
+++ uacv_frontend/src/pages/login.vue
@@ -1,17 +1,20 @@
 <template>
-  <v-container>
-    <div class="text-center">
-      <div class="text-h3 font-weight-bold mb-n1">Login</div>
-    </div>
+  <v-main>
+  <v-container class="background">
+    <div class="login-container">
+      <div class="logo-container">
+        <img src="@/assets/logo.png" alt="Logo" class="logo">
+        <div class="logo-text">UACV</div>
+      </div>
 
-    <div class="text-center mt-5">
-      <v-form>
-        <v-text-field v-model.trim="username" label="Username" />
-        <v-text-field v-model.trim="password" label="Password" type="password" />
-        <v-btn class="block" @click="Login">Login</v-btn>
+      <v-form class="form-container">
+        <v-text-field v-model.trim="username" label="ID" outlined dense />
+        <v-text-field v-model.trim="password" label="Password" type="password" outlined dense />
+        <v-btn class="login-button" @click="Login">Login</v-btn>
       </v-form>
     </div>
   </v-container>
+</v-main>
 </template>
 
 <script setup>
@@ -32,4 +35,50 @@ const Login = function () {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-main {
+  background-color: #093028;
+  height: 100vh;
+  width: 100vw;
+}
+
+.login-container {
+  padding: 20px;
+  border-radius: 8px;
+  width: 300px;
+  margin: 100px auto;
+  text-align: center;
+}
+
+.logo-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.logo {
+  width: 80px;
+  height: auto;
+}
+
+.logo-text {
+  font-family: 'Arial Black', sans-serif;
+  font-size: 40px;
+  color: #d5e0d7;
+  margin-top: 10px;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  color: #d5e0d7;
+}
+
+.login-button {
+  background-color: #a3b18a;
+  color: #ffffff;
+  font-weight: bold;
+  margin-top: 20px;
+}
+</style>
