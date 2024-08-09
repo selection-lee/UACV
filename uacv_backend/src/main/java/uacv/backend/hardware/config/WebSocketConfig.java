49cf3384b9770801aa2d46d97151b3696e726f59
송준혁 <fprhqkrtk303@naver.com>
2024-08-10 00:37:10 +0900

    feat: 맵핑 데이터 송수신용 Websockect 생성 [2/2]
    - `/map` 주제의 데이터 크기가 너무 커 웹소켓 크기 제한에 걸리는 문제 발생
    - 웹소켓 요청 시 8192 Byte 단위로 쪼개 부분 메시지로 보냄으로 해결
    - 웹소켓 주소 변경: `/relay` -> `/socket/mapping`
    
    Issue ID: S11P12C102-335, S11P12C102-336, S11P12C102-337
              S11P12C102-339, S11P12C102-420

--- uacv_backend/src/main/java/uacv/backend/hardware/config/WebSocketConfig.java
+++ uacv_backend/src/main/java/uacv/backend/hardware/config/WebSocketConfig.java
@@ -5,13 +5,15 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import uacv.backend.hardware.handler.MappingWebSocketHandler;
+
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new RelayWebSocketHandler(), "/relay")
+        registry.addHandler(new MappingWebSocketHandler(), "/socket/mapping")
                 .setAllowedOrigins("*");
     }
 }
