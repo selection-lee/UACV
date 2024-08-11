fef960adb6c3367630f5ebf086a9c80240ee383e
송준혁 <fprhqkrtk303@naver.com>
2024-08-11 19:39:24 +0900

    refactor: 분리된 맵핑 데이터 처리 방식 변경
    - 기존 방식: 분리된 메시지의 마지막 Chunk에 `__LAST_CHUNK__`를 추가하여 전송
    - 기존 방식은 메시지의 크기 및 시작, 종료 지점을 정확히 알 수 없었음
      - `__LAST_CHUNK__`가 추가된 마지막 Chunk가 전송되지 않을 경우 메시지 재조립이
        불가능
    - 전송할 데이터를 192 Byte의 헤더 + 8000 Byte 내용으로 분리하여 전송
    - 헤더 내부에 분리된 메시지의 정보를 포함하여 전송
      header = {
        "message_id": 메시지 고유 UUID,
        "data_type": 메시지의 Topic,
        "total_chunks": 분리된 메시지 총 개수,
        "chunk_number": 현재 부분의 번호,
        "chunk_size": 현재 부분의 크기,
        "total_size": 전체 메시지의 크기,
      }
    
    Issue ID: S11P12C102-335, S11P12C102-336, S11P12C102-337
              S11P12C102-339, S11P12C102-420

--- uacv_backend/src/main/java/uacv/backend/hardware/handler/MappingWebSocketHandler.java
+++ uacv_backend/src/main/java/uacv/backend/hardware/handler/MappingWebSocketHandler.java
@@ -2,29 +2,33 @@ package uacv.backend.hardware.handler;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MappingWebSocketHandler extends TextWebSocketHandler {
 
-    private Set<WebSocketSession> clients = Collections.synchronizedSet(new HashSet<>());
-    private StringBuilder messageBuffer = new StringBuilder();
+    private static final int HEADER_SIZE = 192;
 
-    @Override
-    public boolean supportsPartialMessages() {
-        return true;
-    }
+    private Set<WebSocketSession> clients = Collections.synchronizedSet(new HashSet<>());
+    private Map<WebSocketSession, String> dataSender = new ConcurrentHashMap<>();
+    private Map<String, StringBuilder> messageBuffer = new ConcurrentHashMap<>();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        log.debug("Incomming: {}", session);
+        log.debug("Incomming websocket connection: {}", session);
 
         if (!clients.contains(session)) {
             clients.add(session);
@@ -36,34 +40,45 @@ public class MappingWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-
-        log.debug("Receiving message chunk from session {} (Size: {})", session.getId(), message.getPayloadLength());
         String payload = message.getPayload();
 
-        if (payload.endsWith("__LAST_CHUNK__")) {
-            payload = payload.replace("__LAST_CHUNK__", "");
-            messageBuffer.append(payload);
-            log.debug("Received Last Chunk! (Total: {})", messageBuffer.length());
+        String headerJson = payload.substring(0, HEADER_SIZE).trim();
+        Map<String, Object> header = objectMapper.readValue(headerJson, new TypeReference<Map<String, Object>>() {
+        });
 
-            String receivedMessage = messageBuffer.toString();
+        String chunk = payload.substring(HEADER_SIZE);
 
-            for (WebSocketSession ws : clients) {
-                // 세션이 연결되어있고, 세션 ID가 자기 자신이 아닐 경우에 메시지 전송
-                if (ws.isOpen() && !ws.getId().equals(session.getId())) {
-                    ws.sendMessage(new TextMessage(receivedMessage));
-                    log.debug("Message sent to session {}", ws.getId());
+        String messageId = (String) header.get("message_id");
+        String dataType = (String) header.get("data_type");
+        int chunkNumber = (int) header.get("chunk_number");
+        int totalChunks = (int) header.get("total_chunks");
+
+        dataSender.put(session, dataType);
+
+        log.trace("Received chunk for message ID: {}, Chunk: {}/{}, Data Type: {}", messageId, chunkNumber, totalChunks,
+                dataType);
+        messageBuffer.computeIfAbsent(messageId, k -> new StringBuilder()).append(chunk);
+
+        if (chunkNumber == totalChunks) {
+            log.debug("All chunks received for message ID: {} from session {}. Assembling complete message.", messageId,
+                    session.getId());
+            String completeMessage = messageBuffer.remove(messageId).toString();
+
+            for (WebSocketSession client : clients) {
+                if (client.isOpen() && !client.getId().equals(session.getId()) && dataSender.get(client) == null) {
+                    log.debug("Message sent to session {}", client.getId());
+                    client.sendMessage(new TextMessage(completeMessage));
+                } else if (dataSender.get(client) != null) {
+                    log.debug("Session {} is not receiver (Data type: {}), not sending {} data", client.getId(),
+                            dataSender.get(client), dataType);
                 }
             }
-            messageBuffer.setLength(0);
-        } else {
-            messageBuffer.append(payload);
-            log.debug("Partial Message: {} (Total: {})", message, messageBuffer.length());
         }
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        log.debug("Closed: {}", session);
+        log.debug("Closed websocket connection: {}", session);
         clients.remove(session);
     }
 }
