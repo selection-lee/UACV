3143ad830ef1388fa088452150ac4e427446de81
송준혁 <fprhqkrtk303@naver.com>
2024-08-12 00:03:52 +0900

    feat: ROS 클라이언트에게 수신된 주제를 ROS 메시지 형태로 전송
    ROS 메시지 구조:
    {
      "op": "publish",
      "topic": <string>,
      "msg": <json>
    }
    
    Issue ID: S11P12C102-335, S11P12C102-336, S11P12C102-337
              S11P12C102-339, S11P12C102-420

--- uacv_backend/src/main/java/uacv/backend/hardware/handler/MappingWebSocketHandler.java
+++ uacv_backend/src/main/java/uacv/backend/hardware/handler/MappingWebSocketHandler.java
@@ -11,17 +11,23 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MappingWebSocketHandler extends TextWebSocketHandler {
 
+    // ROS 기기에서 수신할 메시지의 헤더 크기
     private static final int HEADER_SIZE = 192;
 
+    // /socket/mapping에 접속한 클라이언트 정보
     private Set<WebSocketSession> clients = Collections.synchronizedSet(new HashSet<>());
+    // 맵핑 데이터 송신자 식별용 해시맵
     private Map<WebSocketSession, String> dataSender = new ConcurrentHashMap<>();
     private Map<String, StringBuilder> messageBuffer = new ConcurrentHashMap<>();
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -42,8 +48,9 @@ public class MappingWebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
 
+        // ROS 기기에서 보낸 메시지인지 확인
         if (payload.length() >= 15 && payload.contains("header_data")) {
-            log.debug("Incomming message has data header!");
+            log.trace("Incomming message has data header!");
             handleMappingData(session, message, payload);
         } else {
             log.debug("Incomming message does not have mapping data header: {}",
@@ -52,18 +59,26 @@ public class MappingWebSocketHandler extends TextWebSocketHandler {
     }
 
     public void handleMappingData(WebSocketSession session, TextMessage message, String payload) throws Exception {
-        String header = payload.substring(0, HEADER_SIZE).trim();
-        Map<String, Object> headerData = objectMapper.readValue(header, new TypeReference<Map<String, Object>>() {});
-        Map<String, Object> mappingData = objectMapper.convertValue(headerData.get("header_data"),
+
+        // scan 주제로 들어오는 값 중 "Infinity"를 처리하기 위한 기능 설정
+        objectMapper.enable(JsonReadFeature.ALLOW_NON_NUMERIC_NUMBERS.mappedFeature());
+
+        // 192 Byte 크기의 헤더 추출 후 빈 공간 제거
+        String strippedHeader = payload.substring(0, HEADER_SIZE).trim();
+
+        // 헤더 데이터 추출
+        Map<String, Object> header = objectMapper.readValue(strippedHeader, new TypeReference<Map<String, Object>>() {});
+        Map<String, Object> headerData = objectMapper.convertValue(header.get("header_data"),
                 new TypeReference<Map<String, Object>>() {});
 
         String chunk = payload.substring(HEADER_SIZE);
 
-        String messageId = (String) mappingData.get("message_id");
-        String dataType = (String) mappingData.get("data_type");
-        int chunkNumber = (int) mappingData.get("chunk_number");
-        int totalChunks = (int) mappingData.get("total_chunks");
+        String messageId = (String) headerData.get("message_id");
+        String dataType = (String) headerData.get("data_type");
+        int chunkNumber = (int) headerData.get("chunk_number");
+        int totalChunks = (int) headerData.get("total_chunks");
 
+        // 맵핑 데이터 송신자의 세션 정보와 보낸 데이터 종류 저장
         dataSender.put(session, dataType);
 
         log.trace("Received chunk for message ID: {}, Chunk: {}/{}, Data Type: {}", messageId, chunkNumber, totalChunks,
@@ -73,12 +88,26 @@ public class MappingWebSocketHandler extends TextWebSocketHandler {
         if (chunkNumber == totalChunks) {
             log.debug("All chunks received for message ID: {} from session {}. Assembling complete message.", messageId,
                     session.getId());
-            String completeMessage = messageBuffer.remove(messageId).toString();
 
+            // 완성된 메시지를 Object로 변환
+            String completeJsonMessage = messageBuffer.remove(messageId).toString();
+            Map<String, Object> deserializedMessage = objectMapper.readValue(completeJsonMessage,
+                    new TypeReference<Map<String, Object>>() {});
+
+            // 주제와 메시지 추출 후 ROS 메시지로 변환
+            String topic = (String) deserializedMessage.get("topic");
+            Map<String, Object> topicMessage = objectMapper.convertValue(deserializedMessage.get("message"),
+                    new TypeReference<Map<String, Object>>() {});
+
+            log.debug("Message {}: Topic: {}, KeySet: {}", messageId, topic, topicMessage.keySet());
+
+            String rosMessage = rosMessageBuilder(topic, topicMessage);
+
+            // 변환된 ROS 메시지를 데이터 수신자(프론트엔드 세션)에게만 전송
             for (WebSocketSession client : clients) {
                 if (client.isOpen() && !client.getId().equals(session.getId()) && dataSender.get(client) == null) {
+                    client.sendMessage(new TextMessage(rosMessage));
                     log.debug("Message sent to session {}", client.getId());
-                    client.sendMessage(new TextMessage(completeMessage));
                 } else if (dataSender.get(client) != null) {
                     log.debug("Session {} is not receiver (Data type: {}), not sending {} data", client.getId(),
                             dataSender.get(client), dataType);
@@ -87,9 +116,29 @@ public class MappingWebSocketHandler extends TextWebSocketHandler {
         }
     }
 
+    public String rosMessageBuilder(String topic, Map<String, Object> topicMessage) throws Exception {
+        log.trace("Building ROS message: Topic: {}, Body: {}", topic, topicMessage);
+
+        // 입력된 메시지의 Key, Value 추출 후 메시지 본문 Object 생성
+        ObjectNode messageBody = objectMapper.createObjectNode();
+        for (String key : topicMessage.keySet()) {
+            JsonNode msg = objectMapper.convertValue(topicMessage.get(key), JsonNode.class);
+            messageBody.set(key, msg);
+        }
+
+        // Topic 발행용 ROS 메시지 생성
+        ObjectNode rosMessage = objectMapper.createObjectNode();
+        rosMessage.put("op", "publish")
+                .put("topic", topic)
+                .set("msg", messageBody);
+
+        return objectMapper.writeValueAsString(rosMessage);
+    }
+
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.debug("Closed websocket connection: {}", session);
         clients.remove(session);
+        dataSender.remove(session);
     }
 }
