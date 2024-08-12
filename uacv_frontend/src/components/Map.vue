922cb24efb2343cfc66f8c27ac42f2a6e44947ea
송준혁 <fprhqkrtk303@naver.com>
2024-08-12 17:50:00 +0900

    feat: 대시보드 맵핑 지도 출력
    - 웹소켓 주소 환경변수화

--- uacv_frontend/src/components/Map.vue
+++ uacv_frontend/src/components/Map.vue
@@ -2,13 +2,10 @@
   <div>
     <h5 class="text-h5 font-weight-bold">MAP</h5>
     <div class="map">
-      <div class="iframe-wrapper">
-        <div class="iframe-container" :style="{ transform: 'scale(' + zoomLevel + ')', transformOrigin: '200px 200px' }">
-          <iframe
-            class="map-iframe"
-            src="http://192.168.100.163:3000/"
-            frameborder="0"
-          ></iframe>
+      <div class="canvas-wrapper">
+        <div class="canvas-container"
+          :style="{ transform: 'scale(' + zoomLevel + ')', transformOrigin: '200px 200px' }">
+            <canvas id="mapCanvas"></canvas>
         </div>
       </div>
       <div class="status">
@@ -16,29 +13,149 @@
       </div>
       <div class="zoom-controls">
         <label for="zoomSlider">Zoom Level:</label>
-        <input
-          id="zoomSlider"
-          type="range"
-          min="1"
-          max="2.5"
-          step="0.1"
-          v-model="zoomLevel"
-        />
+        <input id="zoomSlider" type="range" min="1" max="2.5" step="0.1" v-model="zoomLevel" />
         <span>{{ zoomLevel }}</span>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "Map",
-  data() {
-    return {
-      zoomLevel: 1 // 초기 배율
-    };
+<script setup>
+import { onMounted, ref } from 'vue';
+import ROSLIB from 'roslib';
+
+const zoomLevel = ref(1)
+const SOCKET_API_URL = import.meta.env.VITE_MAPPING_SOCKET_URL
+
+onMounted(() => {
+  var ros = new ROSLIB.Ros({
+    url: `${SOCKET_API_URL}/mapping`
+  });
+
+  ros.on('connection', function () {
+    console.log('Connected to websocket server.');
+  });
+
+  ros.on('error', function (error) {
+    console.log('Error connecting to websocket server:', error);
+  });
+
+  var mapListener = new ROSLIB.Topic({
+    ros: ros,
+    name: '/map',
+    messageType: 'nav_msgs/OccupancyGrid'
+  });
+
+  var scanListener = new ROSLIB.Topic({
+    ros: ros,
+    name: '/scan',
+    messageType: 'sensor_msgs/LaserScan'
+  });
+
+  var canvas = document.getElementById('mapCanvas');
+  var ctx = canvas.getContext('2d');
+
+  var mapData = null;
+  var scanData = null;
+  var mapScale = 3; // 지도 확대 배율
+  var scanScale = 60; // 스캔 확대 배율
+  var offsetY = -30; // 지도 Y 축으로 이동할 픽셀 수
+  var offsetX = 0; // 스캔 X 축으로 이동할 픽셀 수
+  var scanOffsetY = 0; // 스캔 Y 축으로 이동할 픽셀 수
+
+  canvas.width = 400;
+  canvas.height = 400;
+
+  mapListener.subscribe(function (message) {
+    console.log("map: ", message)
+    mapData = message;
+    drawMap();
+  });
+
+  scanListener.subscribe(function (message) {
+    console.log("scan: ", message)
+    scanData = message;
+    drawMap(); // Scan 데이터가 업데이트될 때마다 지도 그리기
+  });
+
+  async function drawMap() {
+    if (!mapData) return;
+
+    var width = mapData.info.width;
+    var height = mapData.info.height;
+    var resolution = mapData.info.resolution;
+    var originX = mapData.info.origin.position.x;
+    var originY = mapData.info.origin.position.y;
+
+    var imageData = ctx.createImageData(width * mapScale, height * mapScale);
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        let value = mapData.data[y * width + x];
+        let color = (value === 100) ? 0 : (value === -1 ? 128 : 255);
+        for (let dy = 0; dy < mapScale; dy++) {
+          for (let dx = 0; dx < mapScale; dx++) {
+            let index = ((width - 1 - x) * mapScale + dx + (y * mapScale + dy) * width * mapScale) * 4;
+            imageData.data[index] = color;
+            imageData.data[index + 1] = color;
+            imageData.data[index + 2] = color;
+            imageData.data[index + 3] = 255; // Alpha
+          }
+        }
+      }
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // 지도 회전 변환
+    const imageBitmap = await createImageBitmap(imageData);
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(Math.PI / 2); // 90도 회전
+    ctx.translate(-imageData.height / 2, -imageData.width / 2);
+    ctx.drawImage(imageBitmap, 0, 0);
+    ctx.restore();
+
+    drawScan(); // 맵을 그린 후 스캔 데이터 그리기
   }
-};
+
+  function drawScan() {
+    if (!scanData) return;
+
+    var width = canvas.width;
+    var height = canvas.height;
+    var angle_min = scanData.angle_min;
+    var angle_increment = scanData.angle_increment;
+    var range_max = scanData.range_max;
+
+    ctx.save(); // 현재 상태 저장
+    ctx.globalCompositeOperation = 'source-over'; // 기존 맵 데이터 위에 그리기
+
+    ctx.fillStyle = 'red';
+
+    scanData.ranges.forEach((range, index) => {
+      if (range < range_max) {
+        const angle = angle_min + index * angle_increment;
+        const x = (range * Math.cos(angle)) * scanScale;
+        const y = (range * Math.sin(angle)) * scanScale;
+
+        // 90도 회전
+        const rotatedX = -y;
+        const rotatedY = x;
+
+        // 상하 반전 추가
+        const flippedX = rotatedX;
+        const flippedY = -rotatedY;
+
+        ctx.beginPath();
+        ctx.arc((canvas.width / 2) + flippedX + offsetX, (canvas.height / 2) + flippedY + scanOffsetY, 1, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    });
+
+    ctx.restore(); // 저장된 상태 복원
+  }
+})
 </script>
 
 <style scoped>
@@ -47,7 +164,7 @@ export default {
   position: relative;
 }
 
-.iframe-wrapper {
+.canvas-wrapper {
   position: relative;
   width: 100%;
   max-width: 600px;
@@ -55,10 +172,11 @@ export default {
   overflow: hidden;
 }
 
-.iframe-container {
+.canvas-container {
   width: 100%;
   height: 100%;
-  transform-origin: 200px 200px; /* 확대/축소 기준점 수정 */
+  /* 확대/축소 기준점 수정 */
+  transform-origin: 200px 200px;
 }
 
 .map-iframe {
